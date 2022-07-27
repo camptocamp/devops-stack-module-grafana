c2ac37127e36c81035d09e63ce9168e407ef3d41
Jérémy Barascut <jeremy.barascut@camptocamp.com>
2022-07-27 11:16:29 +0200

    feat(grafana): init module

---
+++ variables.tf
@@ -0,0 +1,47 @@
+#######################
+## Standard variables
+#######################
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "base_domain" {
+  type = string
+}
+
+variable "argocd_namespace" {
+  type = string
+}
+
+variable "cluster_issuer" {
+  type    = string
+  default = "ca-issuer"
+}
+
+variable "namespace" {
+  type    = string
+  default = "grafana"
+}
+
+variable "helm_values" {
+  description = "Helm values, passed as a list of HCL structures."
+  type        = any
+  default     = []
+}
+
+variable "dependency_ids" {
+  type = map(string)
+
+  default = {}
+}
+
+#######################
+## Module variables
+#######################
+
+variable "grafana" {
+  description = "Grafana settings"
+  type        = any
+  default     = {}
+}
